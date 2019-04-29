8f87b3c3353b58b75366c615b0ac623df954b789
Ritu Gupta <ritugupta@ritugupta-macbookpro.roam.corp.google.com>
2019-04-29 11:11:41 +0530

    Updates

---
+++ precache-manifest.3ec862d89606454fb7a5396d7624b36f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23f25d52407a80c337799cbeb078497e",
+    "url": "/.github.io/campaign_project/index.html"
+  },
+  {
+    "revision": "cce1e244bf3bc80507d6",
+    "url": "/.github.io/campaign_project/static/css/2.c5d966c8.chunk.css"
+  },
+  {
+    "revision": "d9ca8a6060caa577d5d8",
+    "url": "/.github.io/campaign_project/static/css/main.896b492f.chunk.css"
+  },
+  {
+    "revision": "cce1e244bf3bc80507d6",
+    "url": "/.github.io/campaign_project/static/js/2.d2dcd681.chunk.js"
+  },
+  {
+    "revision": "d9ca8a6060caa577d5d8",
+    "url": "/.github.io/campaign_project/static/js/main.6b3093e2.chunk.js"
+  },
+  {
+    "revision": "6ebf49a1b49fbec13dc7",
+    "url": "/.github.io/campaign_project/static/js/runtime~main.984a1dad.js"
+  }
+]);
